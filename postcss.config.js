5aefe8efae43901798202ecafb3f295472651a4e
julieg18 <juliannagal18@gmail.com>
2020-06-10 12:19:05 -0500

    fix: reconfigure postcss  * to handle imgs  * to update vendor prefixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': {},
-    cssnano: {},
-  },
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    parser: false,
+    plugins: {
+      'postcss-import': {},
+      'postcss-url': {},
+      'postcss-preset-env': {},
+      autoprefixer: {},
+      cssnano: {},
+    },
+  };
+}
