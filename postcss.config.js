a7924e81925cdf4ab9263c7840909d4cbb6251e8
julieg18 <juliannagal18@gmail.com>
2020-06-30 12:17:18 -0500

    refactor: delete unneeded code

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    parser: false,
-    plugins: {
-      'postcss-import': {},
-      'postcss-url': {},
-      'postcss-preset-env': {},
-      autoprefixer: {},
-      cssnano: {},
-    },
-  };
-}
+module.exports = {
+  parser: false,
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-preset-env': {},
+    autoprefixer: {},
+    cssnano: {},
+  },
+};
