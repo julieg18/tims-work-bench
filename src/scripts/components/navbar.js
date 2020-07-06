3e4873ed6825fe6bc858d9c8e21b00f2fee82a8b
julieg18 <juliannagal18@gmail.com>
2020-07-06 06:13:05 -0500

    :sparkles: feat: complete code for nav-bar

---
+++ src/scripts/components/navbar.js
@@ -0,0 +1,93 @@
+import {
+  navButton,
+  navLinkList,
+  navBackdrop,
+  navLinks,
+} from '../utils/constants';
+
+let lastKnownScrollPos = 0;
+let ticking = false;
+const sectionPositions = {};
+
+function setSectionPositions() {
+  const gallery = document.querySelector('.gallery');
+  const products = document.querySelector('.products');
+  sectionPositions.info = 0;
+  sectionPositions.products = products.offsetTop;
+  sectionPositions.gallery = gallery.offsetTop;
+  sectionPositions.contact =
+    document.body.scrollHeight - window.innerHeight - 5;
+}
+
+function hasActiveNavLinkChanged(newSectionName) {
+  const activeNavLink = navLinks.find((navLink) =>
+    navLink.classList.contains('nav-bar__link-text_active'),
+  );
+  return !activeNavLink.href.includes(newSectionName);
+}
+
+function activateNavLink(sectionName) {
+  if (!hasActiveNavLinkChanged(sectionName)) return;
+  navLinks.forEach((navLink) => {
+    navLink.classList.remove('nav-bar__link-text_active');
+  });
+  const activeNavLink = navLinks.find((navLink) =>
+    navLink.href.includes(sectionName),
+  );
+  activeNavLink.classList.add('nav-bar__link-text_active');
+}
+
+function activateNavLinks(scrollPos) {
+  const { info, gallery, products, contact } = sectionPositions;
+  const roundedScrollPos = Math.round(scrollPos);
+  let section = '';
+  if (roundedScrollPos >= info && roundedScrollPos < products) {
+    section = 'info';
+  } else if (roundedScrollPos >= products && roundedScrollPos < gallery) {
+    section = 'products';
+  } else if (roundedScrollPos >= gallery && roundedScrollPos < contact) {
+    section = 'gallery';
+  } else {
+    section = 'contact';
+  }
+  activateNavLink(section);
+}
+
+function handleScroll() {
+  lastKnownScrollPos = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      activateNavLinks(lastKnownScrollPos);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+}
+
+function toggleNavList() {
+  navButton.classList.toggle('nav-bar__btn_exit');
+  navLinkList.classList.toggle('nav-bar__links_show');
+  navBackdrop.classList.toggle('backdrop_show');
+}
+
+function removeNavList() {
+  navButton.classList.remove('nav-bar__btn_exit');
+  navLinkList.classList.remove('nav-bar__links_show');
+  navBackdrop.classList.remove('backdrop_show');
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('resize', setSectionPositions);
+window.addEventListener('load', setSectionPositions);
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    const section = e.target.href.split('#')[1];
+    window.location.hash = section;
+    window.scroll(0, sectionPositions[section]);
+    removeNavList();
+  });
+});
+navButton.addEventListener('click', toggleNavList);
---
+++ src/scripts/utils/constants.js
@@ -0,0 +1,15 @@
+const product1Details = [
+  ' - Quality: New, heavy-duty bench',
+  ' - Size: 48, 72, 96, or 120 in. L. x 27 in. W x 37 in. H',
+  ' - Materials: Solid wood yellow pine top',
+  " - 4x4's on the corners and extra supports under the table top and on the corners",
+  " - 4x4's are notched out for strength and stability",
+  ' - Reinforced with #10 x 3.5" deck screws and 3/8" carriage bolts',
+  " - 4x4's notched to fit 2x4s and 2x6s",
+];
+const navButton = document.querySelector('.nav-bar__btn');
+const navLinkList = document.querySelector('.nav-bar__links');
+const navBackdrop = document.querySelector('.backdrop_for_nav-links');
+const navLinks = Array.from(document.querySelectorAll('.nav-bar__link-text'));
+
+export { product1Details, navButton, navLinkList, navBackdrop, navLinks };
